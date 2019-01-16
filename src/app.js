ec76756bb51354845e81938a81e0ee17c2e97347
David Gahnassia <davidgahnassia@gmail.com>
2019-01-16 12:09:32 +0200

    KG-514
    G-Suite plugin

---
+++ src/app.js
@@ -0,0 +1,79 @@
+
+const { google } = require('googleapis');
+
+
+function _authorize(action) {
+    let client_id = action.params.CLIENT_ID;
+    let client_secret = action.params.CLIENT_SECRET;
+    let redirect_uris = ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"];
+
+    const oauth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+    let access_token = action.params.ACCESS_TOKEN;
+    let scope = action.params.SCOPES;
+    oauth2Client.credentials = {
+        access_token: access_token,
+        scope: scope,
+        refresh_token: action.params.REFRESH_TOKEN
+    }
+    return oauth2Client;
+}
+
+function _handleParams(param) {
+    if (typeof param == 'string')
+        return JSON.parse(param);
+    else
+        return param;
+}
+
+function createUser(action) {
+    return new Promise((resolve,reject) => {
+        const user = {
+            name: {
+                familyName: action.params.FAMILY_NAME,
+                givenName: action.params.GIVEN_NAME
+            },
+            password: action.params.PASSWORD,
+            primaryEmail: action.params.PRIMARY_EMAIL
+        }
+    
+        const auth = _authorize(action);
+        const service = google.admin({ version: 'directory_v1', auth });
+        return service.users.insert({ resource: user },(err,res) => {
+            if(err){
+                return reject(err)
+            }
+            if (action.params.GROUPS) {
+                let groups = _handleParams(action.params.GROUPS);
+                return Promise.all(groups.map(group => _addUserToGroup(service, group, { email: user.primaryEmail }))).then(()=>res);
+            }
+            return resolve(res);
+        });
+    })
+}
+
+function _addUserToGroup(service, groupKey, user) {
+    return new Promise((resolve,reject) => {
+        return service.members.insert({ groupKey: groupKey, resource: user },(err,res) => {
+            if(err){
+                return reject(err)
+            }
+            resolve()
+        });
+    })
+}
+
+function addUserToGroup(action) {
+    const auth = _authorize(action);
+    const service = google.admin({ version: 'directory_v1', auth });
+    return _addUserToGroup(service, action.params.GROUP_KEY, {
+        email: action.params.PRIMARY_EMAIL
+    });
+}
+
+module.exports = {
+    createUser:createUser,
+    addUserToGroup:addUserToGroup
+}
+
+
