1e3229dcf8e78848efc19d8241c597a9c7a464db
kadosh1000 <kadosh10001@gmail.com>
2019-06-06 15:59:50 +0300

    * update version to 1.1.0 * remove client id, client secret, scopes, access token, refresh token params * add credentials and super admin email params * use service account credentials to create user

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
 const { google } = require("googleapis");
 
+function _getCredentials(action, settings) {
+  let keysParam = action.params.CREDENTIALS || settings.CREDENTIALS;
+  if (typeof keysParam != 'string') {
+      return keysParam;
+  } else {
+      try {
+          return JSON.parse(keysParam)
+      } catch (err) {
+          throw new Error("Invalid credentials JSON");
+      }
+  }
+}
+
 function _authorize(action, settings) {
-  let client_id = settings.CLIENT_ID || action.params.CLIENT_ID;
-  let client_secret = settings.CLIENT_SECRET || action.params.CLIENT_SECRET;
-  let redirect_uris = ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"];
+  let credentials = _getCredentials(action,settings);
+  let superAdminEmail = action.params.SUPER_ADMIN_EMAIL || settings.SUPER_ADMIN_EMAIL
 
-  const oauth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
+
+  const auth = new google.auth.JWT(
+    credentials.client_email,
+    null,
+    credentials.private_key,
+    ["https://www.googleapis.com/auth/admin.directory.user"],
+    superAdminEmail // The service account needs to impersonate a super admin in your domain
   );
-  oauth2Client.credentials = {
-    access_token: settings.ACCESS_TOKEN || action.params.ACCESS_TOKEN,
-    scope: action.params.SCOPES,
-    refresh_token: settings.REFRESH_TOKEN || action.params.REFRESH_TOKEN
-  };
-
-  return google.admin({
-    version: "directory_v1",
-    auth: oauth2Client
-  });
+  
+// Create the admin directory service  
+  return google.admin({version: 'directory_v1', auth});
 }
 
 function _handleParams(param) {
